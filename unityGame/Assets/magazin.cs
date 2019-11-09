63896daf9d94d6ce43850cd7418d775f37616a59
louis k <letsplaynoobfan@gmail.com>
2019-11-09 20:32:51 +0100

    -Movement -Enemie Inteligence -Camera Movement (shift) -Shooting -Melee

---
+++ unityGame/Assets/magazin.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using TMPro;
+
+public class magazin : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh; //UI Text Object
+    public TextMeshProUGUI textMeshMags; //UI Text Object
+    public bool magEmpty = false;
+
+
+    private float ammoCapacity;
+    private float magCapacity;
+    private float ammoLeft;
+    private float magLeft;
+
+    private float reloadTime;
+
+    void Start()
+    {
+        ammoCapacity = GameObject.Find("spieler").GetComponent<crosshair>().ammoCapacity;
+        magCapacity = GameObject.Find("spieler").GetComponent<crosshair>().magCapacity;
+
+        reloadTime = GameObject.Find("spieler").GetComponent<crosshair>().reloadTime;
+
+         magLeft = magCapacity;
+         ammoLeft = ammoCapacity;
+         updateAmmoCount();
+    }
+    void Update()
+    {
+    }
+
+    public void removeBulletFromMag(int amount)
+    {
+        if (ammoLeft == 0)
+            magEmpty = true;
+
+        ammoLeft = ammoLeft - 1;
+            updateAmmoCount();
+
+        if (ammoLeft == 0)
+            magEmpty = true;
+    }
+
+    public void reload()
+    {
+        StartCoroutine("reloadEnum");
+    }
+    IEnumerator reloadEnum()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        if (magLeft > 0)
+        {
+            ammoLeft = ammoCapacity;
+            magLeft--;
+            updateAmmoCount();
+            magEmpty = false;
+        }
+        StopCoroutine("reloadEnum");
+    }
+
+    public void updateAmmoCount()
+    {
+        textMeshMags.text = "/"+magLeft;
+        textMesh.text = ammoLeft + "/" + ammoCapacity;
+        if (ammoLeft < 10)
+        {
+            textMesh.transform.position = new Vector3(62.5f,31f,0f)*1.5f;
+        }
+        else
+        {
+            textMesh.transform.position = new Vector3(55f, 31f, 0f) * 1.5f;
+        }
+    }
+}
---
+++ unityGame/Assets/melee.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melee : MonoBehaviour
+{
+    public GameObject spieler;
+    private float lastShot = 0;
+
+    public float meleeRange;
+    public float meleeRateMin = 120f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (spieler.GetComponent<crosshair>().isMelee)
+            {
+                if ((lastShot - (Time.time * 1000)) <= -(60000 / meleeRateMin))
+                { 
+                     Collider[] hitInfo = Physics.OverlapSphere(spieler.transform.position + spieler.transform.TransformDirection(new Vector3(0f, 0f, meleeRange)), meleeRange);
+                     int i = 0;
+                     while (i < hitInfo.Length)
+                     {
+                         if (hitInfo[i].name != "boden" && hitInfo[i].name != "spieler" && hitInfo[i].name != "bullet(Clone)")
+                         {
+                             Debug.Log(hitInfo[i].name);
+                         }
+                         i++;
+                     }
+                    lastShot = Time.time * 1000;
+                }
+            }
+        }
+    }
+    
+    /*private void OnDrawGizmosSelected()
+    {
+        Vector3 meleePos = spieler.transform.position + spieler.transform.TransformDirection(new Vector3(0f,0f,meleeRange));
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(meleePos, meleeRange);
+    }*/
+}
